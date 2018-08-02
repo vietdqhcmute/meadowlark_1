e1a17a2c08ad4caa275b81f1573c50379c4b40bb
Viet Do <vietdqhcmute@gmail.com>
2018-08-02 15:14:54 +0700

    Code cleaner with routes

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 var express= require('express');
 var exphdbs= require('express-handlebars').create({defaultLayout:'main'});
-var mfortune = require('./lib/fortune.js');
-
+var routes =require('./lib/routing')
 var app=express();
 
-
+//Configuration
 app.set('port',3300);
 app.engine('handlebars',exphdbs.engine);
 app.set('view engine', 'handlebars');
-
 app.use(express.static(__dirname+'/public'));
 
-app.get('/',function(req,res){
-    res.render('home');
-});
-app.get('/about', function(req,res){
-    res.render('about', {hello:'Hello world', fortune: mfortune.getFortune()});
-});
+//Routing
+routes.getRoute(app);
 
 app.listen(app.get('port'));
