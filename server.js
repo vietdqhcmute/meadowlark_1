819797c6cbd7083ad64f73f1b352cb97f7c46f20
Viet Do <vietdqhcmute@gmail.com>
2018-08-02 14:29:26 +0700

    Tidy code at fortune

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 var express= require('express');
 var exphdbs= require('express-handlebars').create({defaultLayout:'main'});
+var mfortune = require('./lib/fortune.js');
 
 var app=express();
-var fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-    ];
+
 
 app.set('port',3300);
 app.engine('handlebars',exphdbs.engine);
@@ -20,8 +15,7 @@ app.get('/',function(req,res){
     res.render('home');
 });
 app.get('/about', function(req,res){
-    var randomFortune= fortunes[Math.floor(Math.random()* fortunes.length)];
-    res.render('about', {fortune:randomFortune});
+    res.render('about', {hello:'Hello world', fortune: mfortune.getFortune()});
 });
 
 app.listen(app.get('port'));
