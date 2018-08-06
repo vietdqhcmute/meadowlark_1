3eda7fb9892261729a909dceabcf31618678d7fc
Viet Do <vietdqhcmute@gmail.com>
2018-08-06 22:10:27 +0700

    the name 'partials is confict with express', change into 'partialData'

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
-var exphdbs = require('express-handlebars').create({ defaultLayout: 'main' });
+var exphdbs = require('express-handlebars').create({
+	defaultLayout: 'main'
+});
 var router = require('./lib/routing');
+var weatherData = require('./lib/weatherData');
 var app = express();
 
 //Configuration
@@ -8,6 +11,15 @@ app.set('port', 3300);
 app.engine('handlebars', exphdbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
+
+//Weather widget testing
+app.use(function(req, res, next) {
+	if (!res.locals.partialsData) {
+		res.locals.partialsData = {};
+	}
+	res.locals.partialsData.weather = weatherData.getWeatherData();
+	next();
+});
 
 //Routing
 router.getAllRoute(app);
